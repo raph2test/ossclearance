8b0c092eda172abd10841c1a3cf5978a471005a2
jamoreno25 <jamoreno_29@yahoo.com>
2021-07-02 19:57:13 +0800

    initial commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def dashboard(request):
 	return render(request, 'index.html')
+
+def register(request):
+	form = UserCreationForm()
+	context = {'form':form}
+
+	if request.method == 'POST':
+		form = RetireeForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/retirees')
+
+	return render(request, 'register.html', context)
+
+def login(request):
+	return render(request, 'login.html')
